7c8c3ad1743ff041be3e0de1b133660e316d58f4
pjinkim-Mac-PC <pjinkim1215@gmail.com>
2019-08-01 13:59:08 -0700

    first commit of Android Sensors App

---
+++ app/src/main/java/com/pjinkim/sensors_data_logger/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pjinkim.sensors_data_logger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
