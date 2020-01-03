99df5fb09310ef30fae9559dff90069d1fd292fd
pjinkim <pjinkim1215@gmail.com>
2020-01-03 01:30:17 -0800

    add Longhead Mall datasets.

---
+++ WiFi_SfM_core/manual_alignment_Asus_Tango_Lougheed_Mall_2.m
@@ -0,0 +1,103 @@
+%% manual alignment for global inertial frame
+
+switch( expCase )
+    
+    case 1
+        % 20200102_01_Lougheed_Mall_2 (reference inertial frame)
+        yaw = 176.0;
+        tx = 0.0;
+        ty = 0.0;
+        tz = 0;
+        
+    case 2
+        % 20200102_02_Lougheed_Mall_2
+        yaw = 177.8;
+        tx = 0.5;
+        ty = 0.0;
+        tz = 0;
+        
+    case 3
+        % 20200102_03_Lougheed_Mall_2
+        yaw = 176.0;
+        tx = 0.0;
+        ty = 0.0;
+        tz = 0;
+        
+    case 4
+        % 20200102_04_Lougheed_Mall_2
+        yaw = 177.0;
+        tx = 0.0;
+        ty = 0.0;
+        tz = 0;
+        
+    case 5
+        % 20200102_05_Lougheed_Mall_2
+        yaw = 177.5;
+        tx = 0.0;
+        ty = 0.0;
+        tz = 0;
+        
+    case 6
+        % 20200102_06_Lougheed_Mall_2 (??)
+        yaw = -1.0;
+        tx = 9.5;
+        ty = -67.0;
+        tz = 0;
+        
+    case 7
+        % 20200102_07_Lougheed_Mall_2 (??)
+        yaw = -1.0;
+        tx = 9.0;
+        ty = -67.0;
+        tz = 0;
+        
+    case 8
+        % 20200102_08_Lougheed_Mall_2
+        yaw = -0.5;
+        tx = 10.0;
+        ty = -65.0;
+        tz = 0;
+        
+    case 9
+        % 20200102_10_Lougheed_Mall_2
+        yaw = -0.3;
+        tx = -58.5;
+        ty = -97.0;
+        tz = 0;
+        
+    case 10
+        % 20200102_11_Lougheed_Mall_2
+        yaw = -1.0;
+        tx = -57.0;
+        ty = -99.5;
+        tz = 0;
+        
+    case 11
+        % 20200102_12_Lougheed_Mall_2
+        yaw = 89.5;
+        tx = -62.5;
+        ty = -32.0;
+        tz = 0;
+        
+    case 12
+        % 20200102_13_Lougheed_Mall_2
+        yaw = -2.0;
+        tx = 1.5;
+        ty = 1.0;
+        tz = 0;
+        
+    case 13
+        % 20200102_14_Lougheed_Mall_2
+        yaw = 179.0;
+        tx = 1.0;
+        ty = 0.0;
+        tz = 0;
+        
+    case 14
+        % 20200102_15_Lougheed_Mall_2
+        yaw = 179.0;
+        tx = 0.5;
+        ty = -1.5;
+        tz = 0;
+        
+end
--- WiFi_SfM_core/setupParams_WiFi_SfM.m
+++ WiFi_SfM_core/setupParams_WiFi_SfM.m
@@ -10,9 +10,9 @@ switch( expCase )
         datasetPath = 'G:/Smartphone_Dataset/4_WiFi_SfM/Asus_Tango/SFU_ASB_10000';
         
     case 3
-        datasetPath = 'G:/Smartphone_Dataset/4_WiFi_SfM/Asus_Tango/Save_On_Foods';
+        datasetPath = 'G:/Smartphone_Dataset/4_WiFi_SfM/Asus_Tango/Home';
         
     case 4
-        datasetPath = 'G:/Smartphone_Dataset/4_WiFi_SfM/Asus_Tango/Home';
+        datasetPath = 'G:/Smartphone_Dataset/4_WiFi_SfM/Asus_Tango/Lougheed_Mall_2';
         
 end
