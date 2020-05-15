d53a48c865cd7c2478cad8a703407e4d778d3ce4
pjinkim <pjinkim1215@gmail.com>
2020-05-15 10:11:45 -0700

    add wifi constratins in drift correction models.

---
+++ WiFi_SfM_core/EuclideanDistanceResidual_Tango_GoogleFLP_WiFi.m
@@ -0,0 +1,61 @@
+function [residuals] = EuclideanDistanceResidual_Tango_GoogleFLP_WiFi(sensorMeasurements, WiFiSimilarityIndex, X)
+
+%
+numRoninData = size(sensorMeasurements,2);
+TangoVIOLocationSave = cell(1,numRoninData);
+residualGoogleFLP = [];
+for k = 1:numRoninData
+    
+    % unpack sensor measurements
+    TangoPolarVIODistance = sensorMeasurements{k}.TangoPolarVIODistance;
+    TangoPolarVIOAngle = sensorMeasurements{k}.TangoPolarVIOAngle;
+    TangoGoogleFLPIndex = sensorMeasurements{k}.TangoGoogleFLPIndex;
+    TangoGoogleFLPLocation = sensorMeasurements{k}.TangoGoogleFLPLocation;
+    TangoGoogleFLPAccuracy = sensorMeasurements{k}.TangoGoogleFLPAccuracy;
+    
+    
+    % Tango VIO drift correction model
+    startLocation = X((3*k-2):(3*k-1)).';
+    rotation = X(3*k);
+    numTangoVIO = size(TangoPolarVIODistance,2);
+    scale = ones(1,numTangoVIO);
+    bias = zeros(1,numTangoVIO);
+    TangoVIOLocation = DriftCorrectedTangoVIOAbsoluteAngleModel(startLocation, rotation, scale, bias, TangoPolarVIODistance, TangoPolarVIOAngle);
+    TangoVIOLocationSave{k} = TangoVIOLocation;
+    
+    
+    % residuals for Google FLP location
+    TangoEstimatedLocation = TangoVIOLocation(:,TangoGoogleFLPIndex);
+    TangoLocationNormError = vecnorm(TangoEstimatedLocation - TangoGoogleFLPLocation);
+    eachResidualGoogleFLP = max((TangoLocationNormError - TangoGoogleFLPAccuracy), 0);
+    residualGoogleFLP = [residualGoogleFLP, eachResidualGoogleFLP];
+end
+
+
+% WiFi similarity constraints
+numWiFiConstraints = size(WiFiSimilarityIndex,1);
+TangoLocationNormError = zeros(numWiFiConstraints, (numRoninData-1));
+for k = 1:numWiFiConstraints
+    for m = 1:(numRoninData-1)
+        
+        % RoNIN sequence index
+        roninQueryIndex = 1;
+        roninTestIndex = (m+1);
+        
+        % WiFi index
+        queryIndex = WiFiSimilarityIndex(k,roninQueryIndex);
+        testIndex = WiFiSimilarityIndex(k,roninTestIndex);
+        TangoLocationNormError(k,m) = norm(TangoVIOLocationSave{roninQueryIndex}(:,queryIndex) - TangoVIOLocationSave{roninTestIndex}(:,testIndex));
+    end
+end
+TangoLocationNormError = TangoLocationNormError(:).';
+residualWiFi = max((TangoLocationNormError - 5.0), 0);
+
+
+% (3) final residuals for nonlinear optimization
+%residuals = [residualWiFi];
+residuals = [residualGoogleFLP, residualWiFi];
+
+
+end
+
---
+++ WiFi_SfM_core/WiFi_constraints_optimization.m
@@ -0,0 +1,239 @@
+
+%% temporary codes for WiFi similarity based optimization
+
+% between (1) and (2)
+testRoninIndex = [2 7 17 22 25 41];
+numRoninData = size(testRoninIndex,2);
+
+
+% assign partial Tango VIO same trajectories
+partialTangoVIO = cell(1,numRoninData);
+count = 0;
+for k = testRoninIndex
+    count = count + 1;
+    partialTangoVIO{count} = datasetTangoVIO{k};
+end
+
+
+% find the most similar WiFi RSSI vector
+queryTangoRSSI = [partialTangoVIO{1}.RSSI];
+partialTangoVIO{1}(1).WiFiIndex = [];
+for n = 2:numRoninData
+    
+    testTangoRSSI = [partialTangoVIO{n}.RSSI];
+    for k = 1:size(queryTangoRSSI,2)
+        
+        queryRSSI = queryTangoRSSI(:,k);
+        
+        if (sum(queryRSSI == -200) == 1179)
+            continue;
+        else
+            
+            % calculate reward function based on the RSSI vectors
+            numTest = size(testTangoRSSI,2);
+            rewardResult = zeros(1,numTest);
+            for m = 1:numTest
+                rewardResult(m) = computeRewardMetric(queryRSSI, testTangoRSSI(:,m));
+            end
+            [~,maxRewardIndex] = max(rewardResult);
+            
+            % save the result
+            partialTangoVIO{1}(k).WiFiIndex = [partialTangoVIO{1}(k).WiFiIndex, maxRewardIndex];
+        end
+    end
+end
+
+
+% add query RSSI index
+for k = 1:size(partialTangoVIO{1},2)
+    if (~isempty(partialTangoVIO{1}(k).WiFiIndex))
+        partialTangoVIO{1}(k).WiFiIndex = [k, partialTangoVIO{1}(k).WiFiIndex];
+    end
+end
+
+
+%% (1) measurements (constants)
+
+% traditional measurements
+sensorMeasurements = cell(1,numRoninData);
+isOffsetInitialized = false;
+for k = 1:numRoninData
+    
+    % current Tango VIO data
+    TangoVIO = partialTangoVIO{k};
+    
+    
+    % Tango VIO constraints (relative movement)
+    TangoPolarVIO = convertTangoVIOPolarCoordinate(TangoVIO);
+    numTangoVIO = size(TangoPolarVIO,2);
+    
+    
+    % Google FLP constraints (global location)
+    TangoGoogleFLPIndex = [];
+    for m = 1:numTangoVIO
+        if (~isempty(TangoVIO(m).FLPLocationMeter))
+            TangoGoogleFLPIndex = [TangoGoogleFLPIndex, m];
+        end
+    end
+    TangoGoogleFLPLocation = [TangoVIO.FLPLocationMeter];
+    
+    
+    % Google FLP offset for 2D rigid body rotation
+    if (~isOffsetInitialized)
+        isOffsetInitialized = true;
+        offset = TangoGoogleFLPLocation(:,1);
+    end
+    TangoGoogleFLPLocation = TangoGoogleFLPLocation - offset;
+    
+    
+    % sensor measurements from Tango VIO, Google FLP
+    measurements.TangoPolarVIODistance = [TangoPolarVIO.distance];
+    measurements.TangoPolarVIOAngle = [TangoPolarVIO.angle];
+    measurements.TangoGoogleFLPIndex = TangoGoogleFLPIndex;
+    measurements.TangoGoogleFLPLocation = TangoGoogleFLPLocation;
+    measurements.TangoGoogleFLPAccuracy = [TangoVIO.FLPAccuracyMeter];
+    sensorMeasurements{k} = measurements;
+end
+
+
+% construct WiFi constraints
+WiFiSimilarityIndex = [];
+for k = 1:size(partialTangoVIO{1},2)
+    if (~isempty(partialTangoVIO{1}(k).WiFiIndex))
+        WiFiSimilarityIndex = [WiFiSimilarityIndex; partialTangoVIO{1}(k).WiFiIndex];
+    end
+end
+WiFiSimilarityRadius = 7.0;
+
+
+%% (2) model parameters (variables) for Tango VIO drift correction model
+
+% initialize Tango VIO drift correction model parameters
+X_initial = zeros(1,3*numRoninData);
+for k = 1:numRoninData
+    X_initial((3*k-2):(3*k-1)) = sensorMeasurements{k}.TangoGoogleFLPLocation(:,1).';
+end
+
+
+%% (3) nonlinear optimization
+
+% run nonlinear optimization using lsqnonlin in Matlab (Levenberg-Marquardt)
+options = optimoptions(@lsqnonlin,'Algorithm','levenberg-marquardt','Display','iter-detailed','MaxIterations',400);
+[vec,resnorm,residuals,exitflag] = lsqnonlin(@(x) EuclideanDistanceResidual_Tango_GoogleFLP_WiFi(sensorMeasurements, WiFiSimilarityIndex, x),X_initial,[],[],options);
+X_optimized = vec;
+
+
+%
+numRoninData = size(sensorMeasurements,2);
+TangoVIOLocationSave = cell(1,numRoninData);
+for k = 1:numRoninData
+    
+    % unpack sensor measurements
+    TangoPolarVIODistance = sensorMeasurements{k}.TangoPolarVIODistance;
+    TangoPolarVIOAngle = sensorMeasurements{k}.TangoPolarVIOAngle;
+    
+    
+    % Tango VIO drift correction model
+    startLocation = X_optimized((3*k-2):(3*k-1)).';
+    rotation = X_optimized(3*k);
+    numTangoVIO = size(TangoPolarVIODistance,2);
+    scale = ones(1,numTangoVIO);
+    bias = zeros(1,numTangoVIO);
+    TangoVIOLocation = DriftCorrectedTangoVIOAbsoluteAngleModel(startLocation, rotation, scale, bias, TangoPolarVIODistance, TangoPolarVIOAngle);
+    TangoVIOLocationSave{k} = TangoVIOLocation;
+    
+    
+    % plot Tango VIO location
+    distinguishableColors = distinguishable_colors(numDatasetList);
+    figure(10); hold on; grid on; axis equal; axis tight;
+    plot(TangoVIOLocation(1,:),TangoVIOLocation(2,:),'-','color',distinguishableColors(k,:),'LineWidth',1.5); grid on; axis equal;
+    xlabel('X [m]','FontName','Times New Roman','FontSize',15);
+    ylabel('Y [m]','FontName','Times New Roman','FontSize',15);
+    set(gcf,'Units','pixels','Position',[150 60 1700 900]);  % modify figure
+end
+
+
+
+
+
+
+
+
+%%
+
+
+
+%
+h_WiFi = figure(10);
+for k = 1:size(TangoVIO,2)
+    
+    cla;
+    for n = 1:numRoninData
+        
+        % current Tango VIO data
+        TangoVIO = partialTangoVIO{n};
+        TangoVIOLocation = [TangoVIO.location];
+        
+        % plot Tango VIO location
+        distinguishableColors = distinguishable_colors(numDatasetList);
+        plot(TangoVIOLocation(1,:),TangoVIOLocation(2,:),'-','color',distinguishableColors(n,:),'LineWidth',0.5); hold on; grid on; axis equal;
+        xlabel('X [m]','FontName','Times New Roman','FontSize',15);
+        ylabel('Y [m]','FontName','Times New Roman','FontSize',15);
+    end
+    
+    
+    % check WiFi scan RSSI exist or not
+    TangoVIO = partialTangoVIO{1};
+    if (~isempty(TangoVIO(k).WiFiIndex))
+        
+        WiFiIndex = TangoVIO(k).WiFiIndex;
+        for m = 2:numRoninData
+            
+            % 2D query and estimated location
+            queryLocation = TangoVIO(k).location;
+            estimatedLocation = partialTangoVIO{m}(WiFiIndex(m-1)).location;
+            errorLocation = norm(queryLocation - estimatedLocation);
+            midPoint = (queryLocation + estimatedLocation) / 2;
+            
+            % plot current status
+            line([queryLocation(1) estimatedLocation(1)], [queryLocation(2) estimatedLocation(2)],'color','k','LineWidth',2.5);
+            scatter(queryLocation(1),queryLocation(2),800,'m.');
+            scatter(estimatedLocation(1),estimatedLocation(2),800,'k.');
+            text(midPoint(1)+0.2, midPoint(2)+0.2, 0, sprintf('%2.2f (m)', errorLocation),'Color','k','FontSize',11,'FontWeight','bold');
+        end
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+%%
+
+
+%
+% tempTangoRSSI = cell(1,numRoninData);
+% for k = 1:numRoninData
+%
+%     tempRSSI = [];
+%     TangoVIO = tempTangoVIO{k};
+%     for m = 1:size(TangoVIO,2)
+%          if (~isempty(TangoVIO(m).RSSI))
+%
+%
+%          else
+%
+%          end
+%
+%
+%     end
+%
+%
+%
+% end
